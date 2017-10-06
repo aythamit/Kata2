a6c2128064361113d7348161f78aeeb03018926d
Aythami <Entrar@APD-L13>
2017-10-06 13:08:53 +0100

    Primera Version

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,13 +1,27 @@
 
 package kata2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Kata2 {
 
+    public static final int NDATOS = 20;
 
     public static void main(String[] args) {
         
-        //hola
+        ArrayList<Integer> data = new ArrayList();
+        Map<Integer,Integer> histogram = new HashMap();
+        for(int i = 0; i < NDATOS; i++){
+            data.add( (int) (Math.random()* 10));
+        }
+        
+        for(Integer i : data){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
     }
     
 }
